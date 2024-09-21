27cba631f6254e74aad8c150cafbafdb12b64790
Aden Briano <135684831+BrianoAden@users.noreply.github.com>
2024-09-21 13:47:15 -0500

    created html files for website pages and created their corresponding routes

--- project/main.py
+++ project/main.py
@@ -3,10 +3,11 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
-@main.route("/", methods=["GET"])
-def index():
-    return render_template('index.html')
+@main.route("/")
+def landing():
+    return render_template('landing.html')
 
-@main.route('/profile')
+@main.route('/profile', methods=["GET"])
 def profile():
     return render_template('profile.html')
+
