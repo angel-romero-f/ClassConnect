a5c2d4b87c5fac4f6e0d2e55d1236f12783b0b27
angel-romero-f <angelfloromero@gmail.com>
2024-09-21 16:29:28 -0500

    Fix errors

--- project/auth.py
+++ project/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User, Student, Parent, Teacher
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -27,14 +27,7 @@ def login_post():
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
 
-    if Parent.query.filter_by(id=user.id).first():
-        return redirect(url_for('main.parent_dashboard'))
-    elif Student.query.filter_by(id=user.id).first():
-        return redirect(url_for('main.student_dashboard'))
-    elif Teacher.query.filter_by(id=user.id).first():
-        return redirect(url_for('main.teacher_dashboard'))
-    else:
-        return redirect(url_for('main.profile'))
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
--- project/models.py
+++ project/models.py
@@ -10,47 +10,28 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     role = db.Column(db.String(50))
 
-class Parent(User):
-    #model representing parent with child_name
-    id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
-    child_name = db.Column(db.String(100))
+# class Class(db.Model):
+#     #model representing a class with the name of the class, announcements, teacher, and students associated with the class
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+#     #establishes relationships between classes and other models
+#     teacher = db.relationship('Teacher', back_populates='classes')
+#     enrollments = db.relationship('Enrollment', back_populates='class_')
+#     announcements = db.relationship('Announcement', back_populates='class_')
 
-class Student(User):
-    #model representing student with their enrollments and grade level
-    id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
-    #establishes a one-to-many relationship between students and enrollments
-    enrollments = db.relationship('Enrollment', back_populates='student')
-    grade_level = db.Column(db.String(100))
+# class Enrollment(db.Model):
+#     #model with student, class their enrolled in, and their corresponding ids
+#     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), primary_key=True)
+#     class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), primary_key=True)
+#     student = db.relationship('Student', back_populates='enrollments')
+#     class_ = db.relationship('Class', back_populates='enrollments')
 
-class Teacher(User):
-    #model representing a teacher with the classes they teach and the subjects
-    id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
-    #establishes a one-to-many relationship between teacher and classes
-    classes = db.relationship('Class', back_populates='teacher')
-    subject = db.Column(db.String(100))
-
-class Class(db.Model):
-    #model representing a class with the name of the class, announcements, teacher, and students associated with the class
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
-    #establishes relationships between classes and other models
-    teacher = db.relationship('Teacher', back_populates='classes')
-    enrollments = db.relationship('Enrollment', back_populates='class_')
-    announcements = db.relationship('Announcement', back_populates='class_')
-
-class Enrollment(db.Model):
-    #model with student, class their enrolled in, and their corresponding ids
-    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), primary_key=True)
-    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), primary_key=True)
-    student = db.relationship('Student', back_populates='enrollments')
-    class_ = db.relationship('Class', back_populates='enrollments')
-
-class Announcement(db.Model):
-    #model with announcement title, content of announcement, class it's for, time created, and corresponding class id
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    content = db.Column(db.Text)
-    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
-    class_ = db.relationship('Class', back_populates='announcements')
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+# class Announcement(db.Model):
+#     #model with announcement title, content of announcement, class it's for, time created, and corresponding class id
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(200))
+#     content = db.Column(db.Text)
+#     class_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
+#     class_ = db.relationship('Class', back_populates='announcements')
+#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
