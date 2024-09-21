a93513c04c56d2ad5b2b97b8d4b582b58c331a57
angel-romero-f <angelfloromero@gmail.com>
2024-09-21 00:32:22 -0500

    Set up project files

---
+++ project/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from .models import User
+from .db import db
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'put some secret key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
