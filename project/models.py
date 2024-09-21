656c6d751f49566dba30fcbd9a9033b400dfed93
Aden Briano <135684831+BrianoAden@users.noreply.github.com>
2024-09-21 10:20:54 -0500

    Added user models for teacher, student, and parent

---
+++ project/models.py
@@ -0,0 +1,16 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+class Parent(User):
+    child_name = db.Column(db.string(100))
+
+class Student(User):
+    grade_level = db.Column(db.string(100))
+
+class Teacher(User):
+    subject = db.Column(db.String(100))
